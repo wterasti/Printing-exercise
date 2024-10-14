dfb86d6a63d7dd66ecc43155ad0a428def18e4fa
Wilma Terästi <amk1005722@student.hamk.fi>
2024-10-14 10:52:23 -0400

    Initial commit

---
+++ src/PrintApp.java
@@ -0,0 +1,37 @@
+public class PrintApp {
+    public static void main(String[] args) throws Exception {
+        //Alustetaan muuttujat
+        String tekija = "Wilma"; //Aseta oma nimeni tähän
+        int luku1 = 6; // Aseta arvo
+        int luku2 = 18;
+        int tulo;
+        int erotus;
+        int summa;
+        double jako;
+
+        // Tulostetaan konsoliin
+        System.out.println("Hei olen tulostinohjelma.");
+        System.out.print("Ohjelman tekija: ");
+        System.out.println(tekija);
+
+        //System.out.println(luku1); Kommentoitu pois
+        //Tulostetaan konsoliin:"Luku1-muuttujan arvo on 6"
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        //Tulosta konsoliin vielä:"Luku2-muuttujan arvo on 2"
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+
+        //Kerro luku1 ja luku2 ja sijoita tulos tulo-muuttujaan.
+        tulo = luku1 * luku2;
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+        // Lasketaan ja tulostetaan erotus
+        erotus = luku1 - luku2;
+        System.out.println(luku1 + " - " + luku2 + " = " + erotus);
+        // Lasketaan ja tulostetaan summa
+        summa = luku1 + luku2;
+        System.out.println(luku1 + " + " + luku2 + " = " + summa);
+
+        //Lasketaan ja tulostetaan jako
+        jako = (double) luku1/luku2;
+        System.out.println(luku1 + " / " + luku2 + " = " + jako);
+    }
+}
